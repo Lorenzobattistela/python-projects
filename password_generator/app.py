9cdbdd52685fa95c4aaaa0b1e45c5711dc2042b8
Lorenzo Battistela <lorenzowottrich@gmail.com>
2021-08-23 15:23:07 -0300

    Adding all ascii chars

--- password_generator/app.py
+++ password_generator/app.py
@@ -1,5 +1,7 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-import random, string
+import random
+import string
+
 
 def main():
     limit_low = 0
@@ -7,13 +9,13 @@ def main():
     random_number = random.randint(limit_low, limit_high)
     random_word = randomword(10)
     randomized_password = str(random_number) + random_word
-    hashed_password = generate_password_hash(randomized_password)
-    print(hashed_password)
     print(randomized_password)
 
+
 def randomword(length):
-   letters = string.ascii_lowercase #transform letters in ascii values
-   return ''.join(random.choice(letters) for i in range(length))
+    chars = list("".join(chr(x) for x in range(32, 127)))
+    return ''.join(random.choice(chars) for i in range(length))
+
 
 if __name__ == '__main__':
     main()
